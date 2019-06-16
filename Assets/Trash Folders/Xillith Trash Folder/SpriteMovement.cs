2da22f0a8dc4644daaf54cb2d9be47325d663439
sixjedi <sixjedi@gmail.com>
2019-06-15 19:02:37 -0600

    Moving around a map

---
+++ Assets/Trash Folders/Xillith Trash Folder/SpriteMovement.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    enum DirectionMoved { UP, DOWN, LEFT, RIGHT }
+
+    private float MovedSoFar = 0;
+    private int MovedDirection = (int)DirectionMoved.LEFT;
+    
+    
+
+    void Update()
+    {
+        float directionChangeHorizontal = 0;
+        float directionChangeVertical = 0;
+
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            directionChangeVertical = 1;
+
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            directionChangeVertical = -1;
+
+        }
+
+        if (Input.GetAxis("Horizontal")>0) {
+            directionChangeHorizontal = 1;
+
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            directionChangeHorizontal = -1;
+
+        }
+        Vector3 getHorizontalMotion = new Vector3(directionChangeHorizontal, 0.0f, 0.0f);
+        Vector3 getVerticalMotion = new Vector3(0.0f, directionChangeVertical, 0.0f);
+
+        Debug.Log(directionChangeHorizontal+" "+ directionChangeVertical);
+        if (directionChangeHorizontal != 0) transform.position = transform.position + getHorizontalMotion*Time.deltaTime * 2.2f;
+        if (directionChangeVertical != 0) transform.position = transform.position + getVerticalMotion * Time.deltaTime * 2.2f;
+
+
+    }
+}
