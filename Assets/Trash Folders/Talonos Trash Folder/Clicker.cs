b8492886218be067290895757a2db8cc7433e4db
Brennan Smith <Brennan@InterplayLearning.com>
2019-07-02 19:07:31 -0600

    Emissive lighting is a go. Rendering pipeline changed to the LWRP, might cause pink textures.

---
+++ Assets/Trash Folders/Talonos Trash Folder/Clicker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicker : MonoBehaviour
+{
+    public GameObject[] fireworks;
+    int fireworkType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 pos = Random.insideUnitCircle*8;
+            GameObject.Instantiate(fireworks[(fireworkType++)%fireworks.Length], pos, Quaternion.Euler(0,0,1));
+        }
+    }
+}
---
+++ Assets/Trash Folders/Talonos Trash Folder/SampleMonsterSpriter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleMonsterSpriter : MonoBehaviour
+{
+    Renderer r;
+
+    public GameObject hero;
+    public Renderer mapr;
+
+    int subframe = 0;
+    int frame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.r = this.GetComponent<Renderer>();       //Speeds things up to only get the renderer once.
+        r.material = new Material(r.material); //Copies the material. If you don't do this, all monsters with this material will share all properties you set (notably, the frame.)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (subframe++ > 20)
+        {
+            r.material.SetInt("_Frame", frame++);
+            subframe = 0;
+        }
+
+        //Make sure the monster is lit according to where the hero is:
+        r.material.SetVector("_HeroXY", hero.transform.position/10);
+
+        //this shouldn't be tied to the monster, but I wanted to keep all "sample code" in a single script. Anyway, this also sets the map's renderer to recognize where the hero's light is.
+        mapr.material.SetVector("_HeroXY", hero.transform.position/10);
+    }
+}
