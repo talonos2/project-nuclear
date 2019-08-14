16d08d2f01981ce0cc004b94320234df12eef636
Brennan Smith <Brennan@InterplayLearning.com>
2019-08-13 19:28:22 -0600

    Added shaders for props.

---
+++ Assets/Shaders/BlurredBackground.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlurredBackground : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator RecordFrame()
+    {
+        yield return new WaitForEndOfFrame();
+        var texture = ScreenCapture.CaptureScreenshotAsTexture();
+        // do something with texture
+
+        // cleanup
+        Object.Destroy(texture);
+    }
+
+    public void LateUpdate()
+    {
+        StartCoroutine(RecordFrame());
+    }
+
+}
