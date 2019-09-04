ebe46349d7ffc981c748a559a8014c427ae10ed9
Pluribus <Adam.kendall.wright@gmail.com>
2019-09-03 19:41:47 -0600

    script files with some dialogue to test and test environment

---
+++ Assets/Scripts/Dialogue/allowtest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Naninovel;
+
+public class allowtest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RuntimeInitializer.InitializeAsync();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
