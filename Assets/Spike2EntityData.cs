3483f2299e9ded5e921c16af94a3727f332f052e
sixjedi <sixjedi@gmail.com>
2019-09-14 11:32:01 -0600

    Map01 now has working spikes. Spikes are tied to switches properly.

---
+++ Assets/Spike2EntityData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike2EntityData : SpikeController
+{
+
+    private int totalFrames = 7;
+    void Update()
+    {
+        if (!isAnimating || gameData.Paused) { return; }
+        timeSinceLastFrame += Time.deltaTime;
+        if (timeSinceLastFrame >= 1 / AnimationSpeed)
+        {
+            frameNumber += 1;
+            sRender.material.SetInt("_Frame", frameNumber);
+            timeSinceLastFrame = 0;
+            if (frameNumber == totalFrames-1) isAnimating = false;
+        }
+    }
+}
