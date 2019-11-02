3b4ab13d81dc75ee7ea14baff2c85830f6a35dd5
Pluribus <Adam.kendall.wright@gmail.com>
2019-11-02 17:49:52 -0600

    change portrait names, updae prim's algo

--- Assets/Scripts/Maps/BobPrim.cs
+++ Assets/Scripts/Maps/BobPrim.cs
@@ -38,7 +38,7 @@ public class BobPrim : MonoBehaviour
     void Start()
     {
 
-
+/*
         int numNodes = 9;
         int[,] connections = {
         { 0, 1},
@@ -54,11 +54,13 @@ public class BobPrim : MonoBehaviour
         {2, 5},
         {5, 8}};
 
-        bool[] ending = new bool[numNodes];
+        
+
+        bool[] ending = new bool[connections.Length / 2];
 
 
         ending = Iterate(numNodes, connections);
-
+        */
     }
 
     bool[] Iterate(int numNodes, int [,] connections)
@@ -68,7 +70,7 @@ public class BobPrim : MonoBehaviour
         int[] consider = new int[connections.Length];
 
         int i;
-        int u;
+        int wallington = (connections.Length / 2);
         int lead;
         int check;
         int[] considered= new int[5];
@@ -80,10 +82,12 @@ public class BobPrim : MonoBehaviour
 
 
         
-        for (i=0; i<numNodes; i++)
+        for (i=0; i<wallington; i++)
         {
             if (connections[i, 0] == lead || connections[i, 1] == lead)
+            {
                 walls.Add(i);
+            }
         }
 
 
@@ -91,28 +95,68 @@ public class BobPrim : MonoBehaviour
        while (inside.Count<numNodes)
        {
 
-            check = walls[rand.Next(walls.Count)];
+            //check = walls[rand.Next(walls.Count)];
+            check = rand.Next(walls.Count);
+
+ /*           Debug.Log("check is " + check);
+            Debug.Log("connections " + connections.Length);
+            Debug.Log("walls count " + walls.Count);
+            Debug.Log("walls has " + walls[0]);
+            Debug.Log("walls check " + walls[check]);
+
+            Debug.Log("I don't break things " + connections[walls[check], 0]);
+            Debug.Log("I don't break things " + inside.Contains(connections[walls[check], 0]));*/
 
             if (inside.Contains(connections[walls[check], 0]) && inside.Contains(connections[walls[check], 1]))
+            {
+ //               Debug.Log("main if");
                 final[walls[check]] = false;
+            }
             else
             {
                 final[walls[check]] = true;
                 if (inside.Contains(connections[walls[check], 0]))
+                {
+//                    Debug.Log("second if");
+                    Console.WriteLine(connections[walls[check], 1]);
                     inside.Add(connections[walls[check], 1]);
+
+                    for (i = 0; i < wallington; i++)
+                    {
+                        if (connections[i, 0] == connections[walls[check], 1] || connections[i, 1] == connections[walls[check], 1])
+                        {
+                            walls.Add(i);
+                        }
+                    }
+
+                }
                 else
+                {
+ //                   Debug.Log("second else");
+                    Console.WriteLine(connections[walls[check], 0]);
                     inside.Add(connections[walls[check], 0]);
+
+                    for (i = 0; i < wallington; i++)
+                    {
+                        if (connections[i, 0] == connections[walls[check], 0] || connections[i, 1] == connections[walls[check], 0])
+                        {
+                            walls.Add(i);
+                        }
+                    }
+                }
             }
 
 
             walls.Remove(walls[check]);
         }
 
-            
 
 
 
 
+//        Debug.Log("it ends");
+//        for (i = 0; i <= wallington; i++)
+//            Debug.Log("i is " + i + " " + final[i]);
         return final;
     }
 }
