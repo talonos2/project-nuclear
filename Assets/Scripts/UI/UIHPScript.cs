ed752fa22eaf7aa3de07c75f533794174ea3e2dc
Talonos <Talonos@Gmail.com>
2019-11-02 18:02:37 -0600

    HP and MP bars. Yay programmer art.

--- Assets/Scripts/UI/UIHPScript.cs
+++ Assets/Scripts/UI/UIHPScript.cs
@@ -2,21 +2,26 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIHPScript : MonoBehaviour
 {
     TextMeshPro text;
+    public Image bar;
     CharacterStats stats;
     // Start is called before the first frame update
     void Start()
     {
         text = GetComponent<TextMeshPro>();
         stats = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterStats>();
+        text.renderer.sortingOrder = 50;
+        bar.canvas.sortingOrder = 50;
     }
 
     // Update is called once per frame
     void Update()
     {
         text.text = stats.HP + "/" + stats.MaxHP;
+        bar.fillAmount = ((float)stats.HP / (float)stats.MaxHP);
     }
 }
--- Assets/Scripts/UI/UIMPScript.cs
+++ Assets/Scripts/UI/UIMPScript.cs
@@ -2,21 +2,26 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIMPScript : MonoBehaviour
 {
     TextMeshPro text;
+    public Image bar;
     CharacterStats stats;
     // Start is called before the first frame update
     void Start()
     {
         text = GetComponent<TextMeshPro>();
         stats = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterStats>();
+        text.renderer.sortingOrder = 50;
+        bar.canvas.sortingOrder = 50;
     }
 
     // Update is called once per frame
     void Update()
     {
         text.text = stats.mana + "/" + stats.MaxMana;
+        bar.fillAmount = ((float)stats.mana / (float)stats.MaxMana);
     }
 }
