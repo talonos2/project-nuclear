2ba114c970b01f44972dfcda6d2d66e6e1ee86dc
sixjedi <sixjedi@gmail.com>
2019-11-02 18:19:11 -0600

    final boss room puzzle basically finished.

--- Assets/Scripts/Entity Controllers/BridgeController.cs
+++ Assets/Scripts/Entity Controllers/BridgeController.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class BridgeController : DoodadData
 {
-
+    public bool tempBridge;
     public void removePlatform() {
         this.isPlatformTerrain = false;
+        if (tempBridge) {
+            this.gameObject.GetComponentInChildren<SpriteRenderer>().enabled = false;
+            return;
+        }
+        
         this.gameObject.GetComponentInChildren<MeshRenderer>().enabled = false;
     }
     public void addPlatform() {
         this.isPlatformTerrain = true;
+        if (tempBridge)
+        {
+            this.gameObject.GetComponentInChildren<SpriteRenderer>().enabled = true ;
+            return;
+        }
         this.gameObject.GetComponentInChildren<MeshRenderer>().enabled = true;
     }
     // Update is called once per frame
