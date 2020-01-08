b0e80d0ed530e4b61aeee3a394b15ba72763cb7d
sixjedi <sixjedi@gmail.com>
2020-01-07 20:15:22 -0700

    player no longer on top of world

---
+++ Assets/ChooseItemUI.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseItemUI : MonoBehaviour
+{
+    public Text foundItemText;
+    public Text currentItemText;
+    public Text itemKeepText;
+    public Text sendHomeText;
+    public GameObject foundItemSprite;
+    public GameObject newItemSprite;
+    public GameObject equipItemButton;
+    public GameObject sendItemHomeButton;
+    public Canvas chooseItemUiCanvas;
+    private int optionSelected = 0;
+    protected float delayReset = .14f;
+    protected float delayCounter = 0;
+    private CharacterStats playerStats;
+    private InventoryItem itemTypeCheck;
+    private bool pickingItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameState.fullPause || !pickingItem) { return; }
+        
+
+        if (Input.GetButton("SelectNext") || Input.GetButton("SelectPrevious"))
+        {
+            if (delayCounter <= 0)
+            {
+                delayCounter = delayReset;
+                hideButtonSelection();
+                if (optionSelected == 1)
+                {
+                    optionSelected = 0;
+                }
+                else { optionSelected = 1; }
+                showButtonSelection();
+            }
+            else
+            {
+                delayCounter -= Time.deltaTime;
+            }
+        }        
+
+            if (Input.GetButtonDown("Submit"))
+        {
+            if (optionSelected == 0) { EquipItem(); }
+            if (optionSelected == 1) { SendItemToTown(); }
+        }
+    }
+
+    private void showButtonSelection()
+    {
+        if (optionSelected == 0) { equipItemButton.GetComponent<Image>().enabled = true; }
+        if (optionSelected == 1) { sendItemHomeButton.GetComponent<Image>().enabled = true; }
+    }
+
+    private void hideButtonSelection()
+    {
+        if (optionSelected == 0) { equipItemButton.GetComponent<Image>().enabled = false; }
+        if (optionSelected == 1) { sendItemHomeButton.GetComponent<Image>().enabled = false; }
+    }
+
+    private void SendItemToTown()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void EquipItem()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void setupItemChoiceDisplay(CharacterStats playerData, GameObject rolledItem) {
+
+        //chooseItemUiCanvas.(true);
+        pickingItem = true;
+        Debug.Log("Do I arrive");
+        itemTypeCheck = rolledItem.GetComponent<InventoryItem>();
+        SetItemUI(playerData, rolledItem);
+        chooseItemUiCanvas.enabled = true;
+
+
+        if (itemTypeCheck.Weapon)
+        {
+            if (playerData.weapon != null) GameData.Instance.townWeapons.Add(playerData.weapon);
+            playerData.weapon = rolledItem;
+            playerData.setWeaponStats(rolledItem);
+        }
+        else if (itemTypeCheck.Armor)
+        {
+            if (playerData.armor != null) GameData.Instance.townArmor.Add(playerData.armor);
+            playerData.armor = rolledItem;
+            playerData.setArmorStats(rolledItem);
+        }
+        else if (itemTypeCheck.Accessory)
+        {
+            if (playerData.accessory != null) GameData.Instance.townAccessories.Add(playerData.accessory);
+            playerData.accessory = rolledItem;
+            playerData.setAccessoryStats(rolledItem);
+        }
+
+    }
+
+    private void SetItemUI(CharacterStats playerData, GameObject rolledItem)
+    {
+        /*
+          Text foundItemText;
+    public Text currentItemText;
+    public Text itemKeepText;
+    public Text sendHomeText;
+    public Image foundItemSprite;
+    public Image newItemSprite;*/
+
+        //itemTypeCheck = rolledItem.GetComponent<InventoryItem>();
+        //rolledItem
+        int foundItemStat;
+        int oldItemStat;
+        int totalStatChange;
+        String foundItem = "";
+        String oldItemText = "No Item Equipped";
+            
+
+        if (itemTypeCheck.Weapon) {
+            foundItemStat = rolledItem.GetComponent<Weapon>().addAttack;
+            oldItemStat = playerData.weapon.GetComponent<Weapon>().addAttack;
+            totalStatChange = foundItemStat - oldItemStat;
+            if (totalStatChange<=0) foundItem = rolledItem.gameObject.name + ", Attack: " + foundItemStat + "(<color=red>" + totalStatChange + "</color>)";
+                else foundItem = rolledItem.gameObject.name + ", Attack: " + foundItemStat + "(<color=green>+" + totalStatChange + "</color>)";
+            oldItemText = playerData.weapon.gameObject.name + ", Attack: " + oldItemStat;
+        }
+        if (itemTypeCheck.Armor)
+        {
+            foundItemStat = rolledItem.GetComponent<Armor>().addDefense;
+            oldItemStat = playerData.weapon.GetComponent<Armor>().addDefense;
+            totalStatChange = foundItemStat - oldItemStat;
+            if (totalStatChange <= 0) foundItem = rolledItem.gameObject.name + ", Defense: " + foundItemStat + "(<color=red>" + totalStatChange + "</color>)";
+                else foundItem = rolledItem.gameObject.name + ", Defense: " + foundItemStat + "(<color=green>+" + totalStatChange + "</color>)";
+        }
+        if (itemTypeCheck.Accessory) { Debug.Log("acc"); }
+
+        foundItemText.text = foundItem;
+        foundItemSprite.GetComponent<Image>().sprite = rolledItem.GetComponent<InventoryItem>().itemIcon;
+        newItemSprite.GetComponent<Image>().sprite = rolledItem.GetComponent<InventoryItem>().itemIcon;
+        Debug.Log("endlog");
+    }
+}
--- Assets/Scripts/Inventory/RandomChestController.cs
+++ Assets/Scripts/Inventory/RandomChestController.cs
@@ -251,53 +251,31 @@ public class RandomChestController : EntityData
         else if (itemChest)
         {
             GameObject itemFound=itemListData.getRandomCommonItem(rarity);
+            GameObject uiController = GameObject.FindGameObjectWithTag("DungeonUI");
+            //GameObject.Find("Canvas_SelectItem");
+            //foundCanvas.SetActive(true);
+            uiController.GetComponent<ChooseItemUI>().setupItemChoiceDisplay(playerData, itemFound);
+
+            //load item ui and set it up. Okay to set 'destroy'item option. below but will need to 'pause' game upon activation, and unpause on de-activation
+
             InventoryItem itemTypeCheck = itemFound.GetComponent<InventoryItem>();
             //Activate item, aka activate canvas. How do I attach the canvas to the chests?
             //Maybe have the ui canvas self register in the gameData scrip at the start of each room?
             //Both for gabText and for select item things
             //on enable the ui freezes the scene in the background similar to combat, then controlls the things until a selection is made
 
-            if (itemTypeCheck.Weapon)
-            {
-               if (playerData.weapon!=null) gameData.townWeapons.Add(playerData.weapon);
-                playerData.weapon = itemFound;
-                playerData.setWeaponStats(itemFound);
-            }
-            else if (itemTypeCheck.Armor) {
-                if (playerData.armor != null) gameData.townArmor.Add(playerData.armor);
-                playerData.armor = itemFound;
-                playerData.setArmorStats(itemFound);
-            }
-            else if (itemTypeCheck.Accessory){
-                if (playerData.accessory != null) gameData.townAccessories.Add(playerData.accessory);
-                playerData.accessory = itemFound;
-                playerData.setAccessoryStats(itemFound);
-            }
+            
             Instantiate(itemBreaking, this.transform.position + new Vector3(0, .5f, -10), Quaternion.identity, this.transform);
             Destroy(instanciatedObject);
         }
         else if (rareItemChest)
         {
             GameObject rareItemFound = itemListData.getRandomRareItem(rarity);
-            InventoryItem itemTypeCheck = rareItemFound.GetComponent<InventoryItem>();
-            if (itemTypeCheck.Weapon)
-            {
-                if (playerData.weapon != null) gameData.townWeapons.Add(playerData.weapon);
-                playerData.weapon = rareItemFound;
-                playerData.setWeaponStats(rareItemFound);
-            }
-            else if (itemTypeCheck.Armor)
-            {
-                if (playerData.armor != null) gameData.townArmor.Add(playerData.armor);
-                playerData.armor = rareItemFound;
-                playerData.setArmorStats(rareItemFound);
-            }
-            else if (itemTypeCheck.Accessory)
-            {
-                if (playerData.accessory != null) gameData.townAccessories.Add(playerData.accessory);
-                playerData.accessory = rareItemFound;
-                playerData.setAccessoryStats(rareItemFound);
-            }
+            //InventoryItem itemTypeCheck = rareItemFound.GetComponent<InventoryItem>();
+            GameObject foundCanvas = GameObject.Find("Canvas_SelectItem");
+            foundCanvas.SetActive(true);
+            foundCanvas.GetComponent<ChooseItemUI>().setupItemChoiceDisplay(playerData, rareItemFound);
+
             Instantiate(rareItemBreaking, this.transform.position + new Vector3(0, .5f, -10), Quaternion.identity, this.transform);
             Destroy(instanciatedObject);
 
