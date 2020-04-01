52d9a0148305394b0d723481625530a11b0e0a04
Talonos <Talonos@Gmail.com>
2020-03-31 18:37:11 -0600

    Parallax.

---
+++ Assets/Scripts/Maps/ParallaxHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxHandler : MonoBehaviour
+{
+    public float parallaxAmount;
+    public Vector2 parallaxOffset;
+    Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!mainCamera)
+        {
+            mainCamera = Camera.main;
+            if (!mainCamera)
+            {
+                return;
+            }
+        }
+        Vector3 newPosition = mainCamera.transform.position / parallaxAmount;
+        newPosition.z = this.transform.position.z;
+        newPosition += new Vector3(parallaxOffset.x, parallaxOffset.y, 0);
+        this.transform.position = newPosition;
+    }
+}
