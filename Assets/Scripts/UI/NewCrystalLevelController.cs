b4270faf6e22bc58ff46dc2954dffa52c05ede42
Talonos <Talonos@Gmail.com>
2020-04-18 13:42:16 -0600

    End Run stuff (Heavy WIP)

--- Assets/Scripts/UI/NewCrystalLevelController.cs
+++ Assets/Scripts/UI/NewCrystalLevelController.cs
@@ -1,127 +1,148 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class NewCrystalLevelController : MonoBehaviour
 {
-    public bool healthCrystal;
-    public bool manaCrystal;
-    public bool attackCrystal;
-    public bool defenseCrystal;
+    public CrystalType crystalType;
     public GameObject crystalPanel;
     public Text startingCrystals;
-    public Text crystalBonusText;
+    public TextMeshProUGUI crystalBonusText;
     // Start is called before the first frame update
 
     private static int[] crystalTiers = { 50, 150, 350, 650, 1075, 1625, 2300, 3100, 4025, 5075, 6250, 7550, 8975, 10525, 12200, 14000, 15925, 17975, 20150, 22450, 31451 };
     private static int[] crystalUpgrades = { 50, 100, 200, 300, 425, 550, 675, 800, 925, 1050, 1175, 1300, 1425, 1550, 1675, 1800, 1925, 2050, 2175, 2300, 9001 };
 
-    private float timerUntilAnimation=2f;
     private CharacterStats savedStats;
     private int crystalBonusValue;
     private int cTier;
     private int oldCrystals;
     private int newCrystals;
-    //Creates a full bar every 1 second. To increase the time to fill, lower barAnimationPerFrame or increase timerAnimationReset
-    private float barAnimationPerFrame = .025f;
-    private float timerAnimationReset = .015f;
-    private float timerAnimation = 0;
+
+    //How long does it take to fill a bar? A*B^C seconds.
+    public float baseTimePerBar = 2;
+    public float multipierTimePerBar = 1;
+    public float exponentTimePerBar = .65f;
+    public float barAnimationCurveStrength = .65f;
+
     private bool animationFinished;
+
+    public float timerUntilAnimationStarts;
+
+    private float startNumberOfBars;
+    private float targetNumberOfBars;
+
+    private float durationOfFill;
+    private float timeSoFar;
 
     void Start()
     {
         savedStats = GameObject.Find("GameStateData").GetComponent<CharacterStats>();
-        if (healthCrystal) {
-            crystalBonusValue = 20;
-            SetCrystalLevels(GameData.Instance.HealhCrystalTotal);
-            oldCrystals = GameData.Instance.HealhCrystalTotal;
-            newCrystals = savedStats.HealthCrystalsGained;
-                }
-        if (manaCrystal)
+
+        switch (crystalType)
         {
-            crystalBonusValue = 20;
-            SetCrystalLevels(GameData.Instance.ManaCrystalTotal);
-            oldCrystals = GameData.Instance.ManaCrystalTotal;
-            newCrystals = savedStats.ManaCrystalsGained;
-        }
-        if (attackCrystal)
-        {
-            crystalBonusValue = 4;
-            SetCrystalLevels(GameData.Instance.AttackCrystalTotal);
-            oldCrystals = GameData.Instance.AttackCrystalTotal;
-            newCrystals = savedStats.AttackCrystalsGained;
-        }
-        if (defenseCrystal)
-        {
-            crystalBonusValue = 2;
-            SetCrystalLevels(GameData.Instance.DefenseCrystalTotal);
-            oldCrystals = GameData.Instance.DefenseCrystalTotal;
-            newCrystals = savedStats.defenseCrystalsGained;
+            case CrystalType.HEALTH:
+                oldCrystals = GameData.Instance.HealhCrystalTotal;
+                newCrystals = savedStats.HealthCrystalsGained;
+                break;
+            case CrystalType.MANA:
+                oldCrystals = GameData.Instance.ManaCrystalTotal;
+                newCrystals = savedStats.ManaCrystalsGained;
+                break;
+            case CrystalType.ATTACK:
+                oldCrystals = GameData.Instance.AttackCrystalTotal;
+                newCrystals = savedStats.AttackCrystalsGained;
+                break;
+            case CrystalType.DEFENSE:
+                oldCrystals = GameData.Instance.DefenseCrystalTotal;
+                newCrystals = savedStats.defenseCrystalsGained;
+                break;
 
         }
-        //startingCrystals.text = "0/100";
-        // crystalBonusText.text = "+60";
-        // crystalPanel.transform.localScale = new Vector3(.5f,1,1) ;
-        // if (manaCrystal) crystalBonusText.text = "+40";
 
-        //Make functions in CrystalBuffManager to pull the next bonus level from crystals and apply them as needed. 
-        //Make something to slowly fill bar once determined the difference in levels, including when doing more than one level at once. Maybe it does one level at a time, 
-        //aka recalc bar size upon 'load' and upon hitting a new 'tier'. 
+        Debug.Log("Total " + oldCrystals+", new "+ newCrystals);
+        startNumberOfBars = GetNumberOfBars(oldCrystals);
+        targetNumberOfBars = GetNumberOfBars(newCrystals);
+        float barIncrease = startNumberOfBars - targetNumberOfBars;
+
+        SetBarLevel(startNumberOfBars);
+
+        if (oldCrystals==newCrystals)
+        {
+            Debug.Log("Skipping " + crystalType);
+            animationFinished = true;
+            return;
+        }
+
+        Debug.Log(baseTimePerBar + ", " + multipierTimePerBar + ", " + barIncrease + ", " + exponentTimePerBar);
+        durationOfFill = baseTimePerBar * (Mathf.Pow(multipierTimePerBar* barIncrease, exponentTimePerBar));
+    }
+
+    private float GetNumberOfBars(int numCrystals)
+    {
+        int baseCrystalTier = GetCrystalTier(numCrystals);
+        return(numCrystals - crystalTiers[baseCrystalTier]) / (crystalTiers[baseCrystalTier]- crystalTiers[baseCrystalTier+1]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (animationFinished) { return; }
-        timerUntilAnimation -= Time.deltaTime;
-        if (timerUntilAnimation >= 0) { return; }
+        if (animationFinished)
+        {
+            return;
+        }
+        timerUntilAnimationStarts -= Time.deltaTime;
+        if (timerUntilAnimationStarts >= 0)
+        {
+            return;
+        }
         AnimateCrystalBarsFilling();
     }
 
     private void AnimateCrystalBarsFilling()
     {
-        if (timerAnimation >= 0) {
-            timerAnimation -= Time.deltaTime;
-            return;
+        timeSoFar += Time.deltaTime;
+        if (timeSoFar >= durationOfFill)
+        {
+            animationFinished = true;
+            timeSoFar = durationOfFill;
         }
-        else { timerAnimation = timerAnimationReset; }
 
-        if (newCrystals > 0) {
-            int crystalsToAdd = (int)(crystalUpgrades[cTier] * barAnimationPerFrame);
-            if (crystalsToAdd+ oldCrystals > crystalTiers[cTier]) {
-                crystalsToAdd = crystalTiers[cTier] - oldCrystals;
-            }
-            if (crystalsToAdd > newCrystals) {
-                crystalsToAdd = newCrystals;
-            }
-            oldCrystals += crystalsToAdd;
-            newCrystals -= crystalsToAdd;
-            SetCrystalLevels(oldCrystals);
-        }
-        else { animationFinished = true; }
-        
+        Debug.Log(durationOfFill + ", " + timeSoFar);
+        float amountThrough = timeSoFar/durationOfFill;
+
+        amountThrough = Mathf.Pow(amountThrough, barAnimationCurveStrength);
+
+        float currentBarAmount = startNumberOfBars + (targetNumberOfBars - startNumberOfBars) * amountThrough;
+
+        SetBarLevel(currentBarAmount);
     }
 
-    private void SetCrystalLevels(int crystalTotal)
+    private void SetBarLevel(float barAmount)
     {
-        cTier = GetCrystalTier(crystalTotal);
-        string bonusText = "+" + cTier* crystalBonusValue;
+        int fullBars = Mathf.FloorToInt(barAmount);
+        float remainderBars = barAmount % 1.0f;
+        Debug.Log(barAmount);
+        int crystalAmount = Mathf.RoundToInt(Mathf.Lerp(crystalTiers[fullBars], crystalTiers[fullBars + 1], remainderBars));
+
+        string bonusText = "+" + fullBars * crystalBonusValue;
         crystalBonusText.text = bonusText;
-        int crystalsInTier = crystalTotal-(crystalTiers[cTier]- crystalUpgrades[cTier]);
+
+        int crystalsInTier = crystalAmount - (crystalTiers[cTier] - crystalUpgrades[cTier]);
         int crystalsNeededToLevel = crystalUpgrades[cTier];
         string crystalTotalText = "" + crystalsInTier + "/" + crystalsNeededToLevel;
         startingCrystals.text = crystalTotalText;
-        crystalPanel.transform.localScale = new Vector3((float)crystalsInTier/crystalsNeededToLevel, 1, 1);
-
+        crystalPanel.transform.localScale = new Vector3((float)crystalsInTier / crystalsNeededToLevel, 1, 1);
     }
 
     //Setup Crystal Buffs
 
     public static void SetCrystalBuffs()
     {
-        CharacterStats savedStats= GameObject.Find("GameStateData").GetComponent<CharacterStats>();
+        CharacterStats savedStats = GameObject.Find("GameStateData").GetComponent<CharacterStats>();
         GameData gameData = GameData.Instance;
         gameData.AttackCrystalTotal += savedStats.AttackCrystalsGained;
         savedStats.AttackCrystalsGained = 0;
