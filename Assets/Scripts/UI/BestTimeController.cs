fda529a7bc511bd000f23f5a0e1ef3a5f376ac8b
Talonos <Talonos@Gmail.com>
2020-04-21 18:29:31 -0600

    Little slider now shows when you climbed down to certain floors, clean scripts out of base asset folder.

---
+++ Assets/Scripts/UI/BestTimeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BestTimeController : MonoBehaviour
+{
+    public TextMeshProUGUI[] bestTimesTexts;
+    public Image[] bestTimesMarkers;
+    public Image bestTimeSlider;
+
+
+    public float delay;
+    public float duration;
+
+    public float startTimeOffset;
+    public float endTimeOffset;
+    public float y;
+
+    public int floorsDroppedOff;
+
+    public FloorDropoffPrefab floorDropoffPrefab;
+
+    private float timeSoFar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Fill the thing with debug data if none exists. This is TEST CODE:
+        if (GameData.Instance.timer==0)
+        {
+            //Died after 0 seconds? Should be impossible.
+            Debug.LogError("WARNING: Either you're testing the BestTimeController by running the end run screen directly, or something had gone very wrong!");
+            GameData.Instance.timesThisRun = new []{ 12,22,32, 50, 93.4f, 95.2f, 195.3f, 210,250,300,350,475,550,0,0,0,0,0,0,0};
+        }
+        //End test code.
+
+        bestTimeSlider.transform.localPosition = new Vector3(startTimeOffset, y, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSoFar += Time.deltaTime;
+        if (timeSoFar>delay)
+        {
+            if (timeSoFar < delay + duration)
+            {
+                float t = (timeSoFar - delay) / duration;
+                bestTimeSlider.transform.localPosition = new Vector3(Mathf.Lerp(startTimeOffset, endTimeOffset, t), y, 0);
+                float quickenedTimeTakenSoFar = (timeSoFar - delay) * (600 / duration);
+                Debug.Log(quickenedTimeTakenSoFar + ", " + GameData.Instance.timesThisRun[floorsDroppedOff]);
+                if (quickenedTimeTakenSoFar >= GameData.Instance.timesThisRun[floorsDroppedOff]&& GameData.Instance.timesThisRun[floorsDroppedOff]!=0)
+                {
+                    float tForDropoff = GameData.Instance.timesThisRun[floorsDroppedOff] / 600f;
+                    float dropoffx = Mathf.Lerp(startTimeOffset, endTimeOffset, tForDropoff);
+                    DropOffFloor(floorsDroppedOff, dropoffx);
+                    floorsDroppedOff++;
+                }
+                
+            }
+        }
+    }
+
+    private void DropOffFloor(int floorsDroppedOff, float dropoffx)
+    {
+        Debug.Log("Floor dropped off!");
+        FloorDropoffPrefab dropped = GameObject.Instantiate<FloorDropoffPrefab>(floorDropoffPrefab);
+        dropped.GetComponent<RectTransform>().SetParent(this.GetComponent<RectTransform>());
+        dropped.GetComponent<RectTransform>().localPosition = new Vector3(dropoffx, y, 0);
+        dropped.Initialize();
+    }
+}
---
+++ Assets/Scripts/UI/FloorDropoffPrefab.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloorDropoffPrefab : MonoBehaviour
+{
+    public float timeToFadeIn;
+    private float timeSoFar;
+    public Vector3 startScale = new Vector3 (.5f,2.5f,1);
+    private bool animDone = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animDone)
+        {
+            return;
+        }
+        timeSoFar += Time.deltaTime;
+        if (timeSoFar>=timeToFadeIn)
+        {
+            animDone = true;
+            this.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+            this.GetComponent<RectTransform>().localScale = Vector3.one;
+            return;
+        }
+        this.GetComponent<Image>().color = new Color(1, 1, 1, timeSoFar/timeToFadeIn);
+        this.GetComponent<RectTransform>().localScale = Vector3.Lerp(startScale, Vector3.one, timeSoFar / timeToFadeIn);
+    }
+
+    internal void Initialize()
+    {
+        this.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+        this.GetComponent<RectTransform>().localScale = startScale;
+    }
+}
