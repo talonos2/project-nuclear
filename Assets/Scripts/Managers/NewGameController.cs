17b60b755fab4c1e046ace106114d68f192a5640
Talonos <Talonos@Gmail.com>
2020-05-02 13:53:41 -0600

    Whoops, little selectiony thing.

--- Assets/Scripts/Managers/NewGameController.cs
+++ Assets/Scripts/Managers/NewGameController.cs
@@ -82,7 +82,7 @@ public class NewGameController : MonoBehaviour
     {
         //TODO: Skip continue and load game if there's no game to load or continue. (Repeat check here because of mouse.)
         currentMenuOptionSelected = menuOptionSelected;
-        selectionMarker.transform.position = new Vector3(-70, -110 - 20 * menuOptionSelected, 0);
+        selectionMarker.GetComponent<RectTransform>().localPosition = new Vector3(-70, -110 - 20 * menuOptionSelected, 0);
 
         for (int x = 0; x < menuOptions.Length; x++)
         {
