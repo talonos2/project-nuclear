c3ed3585a7c47e99b7d8a7f10232b103d5a1e4e9
sixjedi <sixjedi@gmail.com>
2020-08-14 21:34:13 -0600

    Victory scene finished except any text we want on it

---
+++ Assets/Scripts/UI/UIDefense.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIDefense : MonoBehaviour
+{
+    TextMeshPro text;
+    CharacterStats stats;
+    void Start()
+    {
+        text = GetComponent<TextMeshPro>();
+        stats = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "" + stats.defense;
+    }
+}
