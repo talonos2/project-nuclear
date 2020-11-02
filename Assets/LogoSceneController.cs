4359e3a52de3bf4f550a55f1cad05df2a2eaf22f
sixjedi <sixjedi@gmail.com>
2020-11-02 13:54:28 -0700

    logo scene now in game

---
+++ Assets/LogoSceneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoSceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FadeOut fadeout = GameObject.Instantiate<FadeOut>(Resources.Load<FadeOut>("Fade Out Plane"));
+        fadeout.InitNext("TitleScreen", .2f);
+        //SceneManager.LoadScene("TitleScreen");
+    }
+
+
+}
