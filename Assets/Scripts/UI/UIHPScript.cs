815c048d888a13996e905cc718868d63a9750c25
Talonos <Talonos@Gmail.com>
2020-12-17 14:12:08 -0700

    Healing flashes.

--- Assets/Scripts/UI/UIHPScript.cs
+++ Assets/Scripts/UI/UIHPScript.cs
@@ -9,10 +9,14 @@ public class UIHPScript : MonoBehaviour
     private float timeSoFar;
     private int lastBeepAt;
 
+    private int oldHP;
+    private float timeSinceFlash = 100f;
+
     public float beepFrequency = .8f;
 
     TextMeshPro text;
     public Image bar;
+    public Image flashBar;
     public SpriteRenderer backgroundBlobToFlash;
     CharacterStats stats;
     // Start is called before the first frame update
@@ -20,6 +24,7 @@ public class UIHPScript : MonoBehaviour
     {
         text = GetComponent<TextMeshPro>();
         stats = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterStats>();
+        oldHP = stats.HP;
     }
 
     // Update is called once per frame
@@ -29,10 +34,30 @@ public class UIHPScript : MonoBehaviour
 
         text.text = stats.HP + "/" + stats.MaxHP;
         bar.fillAmount = ((float)stats.HP / (float)stats.MaxHP);
-        if (stats.HP <= stats.MaxHP / 5)
+        flashBar.fillAmount = ((float)stats.HP / (float)stats.MaxHP);
+
+        if (stats.HP>oldHP+stats.MaxHP*.1f|| (oldHP != stats.MaxHP&&stats.HP==stats.MaxHP))
+        {
+            Debug.Log("Constant: " + oldHP + ", " + (stats.HP + stats.MaxHP * .1f) + ", " + (oldHP != stats.MaxHP) + ", " + (stats.HP == stats.MaxHP));
+            timeSinceFlash = 0;
+        }
+
+        timeSinceFlash += Time.deltaTime;
+
+        oldHP = stats.HP;
+
+
+        if (timeSinceFlash < .5f)
+        {
+            bar.color = Color.white;
+            backgroundBlobToFlash.color = Color.black;
+            flashBar.color = new Color(1, 1, 1, Mathf.Pow(1 - (timeSinceFlash * 2), 2));
+        }
+        else if (stats.HP <= stats.MaxHP / 5)
         {
             float t = Mathf.Sin(timeSoFar*4) / 2 + .5f;
             bar.color = new Color(1, t, t);
+            flashBar.color = new Color(1, 1, 1, 0);
             backgroundBlobToFlash.color = new Color((1-t)/3, 0, 0);
             int beepNum = (int)(timeSoFar / beepFrequency);
             if (lastBeepAt == beepNum-1)
@@ -45,6 +70,7 @@ public class UIHPScript : MonoBehaviour
         {
             bar.color = Color.white;
             backgroundBlobToFlash.color = Color.black;
+            flashBar.color = new Color(1, 1, 1, 0);
         }
     }
 }
