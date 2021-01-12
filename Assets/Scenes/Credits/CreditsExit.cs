810c5f1835982906d2e2cc74d8b94d000250811b
Pluribus <Adam.kendall.wright@gmail.com>
2021-01-12 17:52:07 -0500

    alternate credit scene

---
+++ Assets/Scenes/Credits/CreditsExit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Naninovel;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CreditsExit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CloseCredits()
+    {
+        SceneManager.UnloadSceneAsync("SimpleCredits");
+    }
+}
