d782d1c3018265bfc8246aba643263a0e90e8a57
Talonos <Talonos@Gmail.com>
2021-02-02 19:02:20 -0700

    Fades

--- Assets/Scripts/Managers/LogoSceneController.cs
+++ Assets/Scripts/Managers/LogoSceneController.cs
@@ -9,42 +9,26 @@ public class LogoSceneController : MonoBehaviour
     AsyncOperation loadingTitleScreen;
     private float minDelayUntilNextScene = 2.5f;
     public AudioListener listenerToSilence;
-    private bool fadingOut;
-    private FadeOut fadeout;
-    private bool initFadeout;
 
     void Start()
     {
-        FWInputManager.Instance.GetKeyDown(InputAction.ACTIVATE) ;
-       // FadeOut fadeout = GameObject.Instantiate<FadeOut>(Resources.Load<FadeOut>("Fade Out Plane"));
-
-        //fadeout.InitNext("TitleScreen", .2f);
+        FWInputManager.Instance.GetKeyDown(InputAction.ACTIVATE);
         loadingTitleScreen = SceneManager.LoadSceneAsync("TitleScreen",LoadSceneMode.Additive);       
         loadingTitleScreen.allowSceneActivation = false;
-        //SceneManager.LoadScene("TitleScreen");
     }
 
     private void Update()
     {
         minDelayUntilNextScene -= Time.deltaTime;
         if (minDelayUntilNextScene < 0) {
-            if (loadingTitleScreen.isDone) {
-                if (!initFadeout) {
-                    //FadeOut fadeout = GameObject.Instantiate<FadeOut>(Resources.Load<FadeOut>("Fade Out Plane"));
-                    //fadeout.attachToGUI(this.transform.parent.GetComponent<Canvas>());
-                    //fadeout.InitScenelessFadeout(.2f);
-                    //initFadeout = true;
-                    //fadeout = GameObject.Instantiate<FadeOut>(Resources.Load<FadeOut>("Fade Out Plane"));
-                    //fadeout.InitScenelessFadeout(5.5f);
-                    //fadingOut = true;
-                    //initFadeout = true;
-                }
+            if (loadingTitleScreen.isDone)
+            {
+
                 GameData.Instance.startSceneLoaded = true;
                 listenerToSilence.enabled = false;
                 SceneManager.UnloadSceneAsync("LogoScreen");
                 loadingTitleScreen.allowSceneActivation = true;
 
-                //SceneManager.SetActiveScene(SceneManager.GetSceneByName("TitleScreen"));
             }
         }
 
@@ -53,19 +37,6 @@ public class LogoSceneController : MonoBehaviour
             SceneManager.LoadScene("TitleScreen");
             GameData.Instance.startSceneLoaded = true;
         }
-
-        /*if (fadingOut) {
-            if (fadeout.scenelessFadeOutFinished())
-             {
-            GameData.Instance.startSceneLoaded = true;
-            listenerToSilence.enabled = false;
-            SceneManager.UnloadSceneAsync("LogoScreen");
-            loadingTitleScreen.allowSceneActivation = true;
-            }
-        }*/
-
-
-        //if (FWInputManager.Instance.GetKeyDown(InputAction.ACTIVATE)) { Debug.Log("keypressed in Logos"); }
 
     }
 
